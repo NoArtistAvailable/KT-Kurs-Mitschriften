cce98a7a2f62a27916b1890146f8bcde0806b818
zach <felix.reinemann@gmail.com>
2024-09-03 09:05:17 +0200

    day1

---
+++ KT240902/Project1/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//Kurs Helloworld
+//int main() {
+//	std::cout << "Hello World" << std::endl;
+//	return 0;
+//}
+
+//Uebungsaufgabe A
+
+using namespace std;
+
+int main() {
+	int myAge;
+	cout << "Enter your age: ";
+	cin >> myAge;
+	std::cout << (myAge * 2);
+}
