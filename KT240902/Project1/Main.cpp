4cf3f372e795a6672a3aab2d87123141078f7340
zach <felix.reinemann@gmail.com>
2024-09-03 11:32:21 +0200

    multi projects

--- KT240902/Project1/Main.cpp
+++ KT240902/Project1/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 //Kurs Helloworld
 //int main() {
@@ -8,11 +9,47 @@
 
 //Uebungsaufgabe A
 
-using namespace std;
+
+// A)
+//int main() {
+//	int myAge;
+//	cout << "Enter your age: ";
+//	cin >> myAge;
+//	std::cout << (myAge * 2);
+//}
 
 int main() {
-	int myAge;
-	cout << "Enter your age: ";
-	cin >> myAge;
-	std::cout << (myAge * 2);
+	int num;
+
+	cout << "Bitte gib eine Zahl ein: ";
+	cin >> num;
+
+	cout << "\aDu hast folgenden Wert eingegeben Wert eingegeben: " << num << endl;
+
+	
+	// implizit
+	int i = 10;
+	double d = i;
+
+	// C-Style Cast
+	d = 6.987;
+	i = (int)d;
+
+	// functional casting
+	d = 6.879;
+	i = int(d);
+
+	// static cast
+	i = static_cast<int>(d);
+
+	// dynamic cast
+	class Basis {virtual void func() {} };
+	class Ableitung : public Basis{};
+
+	Basis* b = new Ableitung();
+	Ableitung* a = dynamic_cast<Ableitung*>(b);
+
+	cout << "Typecast D " << d << "I " << i << endl;
+
+	return num;
 }
---
+++ KT240902/Project2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum1, sum2;
+    cout << "Gib zwei Zahlen ein: ";
+    cin >> sum1 >> sum2;
+    cout << "Summe: " << sum1 + sum2 << endl;
+}
+
